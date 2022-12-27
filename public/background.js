0ffdd449a9d6ae7e8b372adaaf7008d33874e7e2
Piotr Maj <piotrmaj@MBP-Piotr.Dlink>
2022-12-27 10:41:15 +0100

    feat: added some example chat queries

--- public/background.js
+++ public/background.js
@@ -3,15 +3,22 @@
 chrome.runtime.onMessage.addListener((req) => {
   if (req == "linkchat") {
     const queries = [
+      "What+is+the+difference+between+AI+and+machine+learning",
       "What+is+the+best+poem",
       "What+is+the+capital+of+France",
       "What+is+the+meaning+of+life",
       "Tell+me+a+joke",
-      "What+is+the+difference+between+AI+and+machine+learning",
       "How+can+I+learn+more+about+artificial+intelligence",
-      "What+are+the+best+books+to+read+about+technology",
       "How+can+I+get+started+with+coding",
       "What+are+the+major+trends+in+the+tech+industry+right+now",
+      "Can+you+provide+a+brief+summary+of+a+recent+news+article",
+      "Write+a+sequel+about+forrest+gump+movie",
+      "Simple+ideas+to+decorate+my+home",
+      "Write+a+news+story+about+the+first+human+to+land+on+mars",
+      "Develop+rules+for+a+game+that+combines+monopoly+and+chess",
+      "Write+a+song+about+a+snake+and+a+mouse+that+are+friends+Eminem+style",
+      "Act+as+a+linux+terminal.+I+will+type+commands+and+you+will+reply+with+what+the+terminal+should+show",
+      "What+are+the+best+books+to+read+about+technology",
     ];
 
     const index = Math.floor(Math.random() * 100) % queries.length;
