86efb1e4c2187b1d909fa824803247b3c0d529df
Clint Edwards <ce@buildmidwestern.com>
2024-07-30 16:07:52 -0600

    [Feat] Removed almost everything

--- public/background.js
+++ public/background.js
@@ -22,63 +22,16 @@ chrome.runtime.onMessage.addListener((req) => {
 
   const index = Math.floor(Math.random() * 100) % queries.length;
 
-  const { domain, shouldOpenNewTab } = req;
+  const { shouldOpenNewTab } = req;
 
-  if (domain === "linkchat") {
+  if (shouldOpenNewTab) {
     chrome.tabs.create({
       url: "https://you.com/search?q=" + queries[index] + "&tbm=youchat",
     });
-  } else if (domain === "linkwrite") {
-    chrome.tabs.create({ url: "https://you.com/search?q=%40write" });
-  } else if (domain === "linkcode") {
-    chrome.tabs.create({ url: "https://you.com/search?q=%40code" });
-  } else if (domain === "linkdraw") {
-    chrome.tabs.create({ url: "https://you.com/search?q=%40draw" });
-  } else if (domain === "code") {
-    chrome.declarativeNetRequest.updateEnabledRulesets({
-      enableRulesetIds: ["ruleset_1"],
-      disableRulesetIds: ["ruleset_2", "ruleset_3", "ruleset_4"],
-    });
-    if (shouldOpenNewTab) {
-      chrome.tabs.create({ url: "https://code.you.com/" });
-    }
-  } else if (domain === "youchat") {
-    chrome.declarativeNetRequest.updateEnabledRulesets({
-      enableRulesetIds: ["ruleset_3"],
-      disableRulesetIds: ["ruleset_1", "ruleset_2", "ruleset_4"],
-    });
-    if (shouldOpenNewTab) {
-      chrome.tabs.create({
-        url: "https://you.com/search?q=" + queries[index] + "&tbm=youchat",
-      });
-    }
-  } else if (domain === "google") {
-    chrome.declarativeNetRequest.updateEnabledRulesets({
-      enableRulesetIds: ["ruleset_4"],
-      disableRulesetIds: ["ruleset_1", "ruleset_2", "ruleset_3"],
-    });
-    if (shouldOpenNewTab) {
-      chrome.tabs.create({ url: "https://www.google.com/" });
-    }
-  } else {
-    chrome.declarativeNetRequest.updateEnabledRulesets({
-      enableRulesetIds: ["ruleset_2"],
-      disableRulesetIds: ["ruleset_1", "ruleset_3", "ruleset_4"],
-    });
-    if (shouldOpenNewTab) {
-      chrome.tabs.create({ url: "https://you.com/" });
-    }
   }
 });
 
 chrome.runtime.setUninstallURL("https://about.you.com/exit-survey");
-
-// https://developer.chrome.com/docs/extensions/reference/runtime/#event-onInstalled
-chrome.runtime.onInstalled.addListener((request) => {
-  if (request.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    chrome.tabs.create({ active: true, url: "https://about.you.com/welcome/" });
-  }
-});
 
 // Let the you.com website detect whether the extension is installed
 // Source: https://stackoverflow.com/a/21043701
