43b8a3c95bb1fde5c35ed60cde33ead9ebfdf276
Geoffrey Long <geoffrey@you.com>
2025-02-19 21:37:25 -0500

    [TD] Fix dependabot issues (#24)

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
